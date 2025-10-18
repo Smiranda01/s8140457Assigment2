328597c18df6b9edcd503d44d13dfbeb6b72894e
Santiago Miranda <santiago.miranda.rodriguez21@gmail.com>
2025-10-18 21:26:49 +1100

    Add gradle dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,17 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "2.0.0"
+        id("com.google.dagger.hilt.android") version "2.51.1"
+        id("androidx.navigation.safeargs.kotlin") version "2.8.3"
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -21,4 +22,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "s8140457_assignment2"
 include(":app")
- 
